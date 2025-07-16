f89dc23204e34cc1db26cefa187cd0d4aac7ff9f
ounjuu <koj1555@naver.com>
2025-07-16 11:37:36 +0900

    Feat: 로그인 정보 저장 수정

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -28,7 +28,12 @@ export class AuthService {
   }
 
   async login(user: User) {
-    const payload = { email: user.email, sub: user.id, role: user.role };
+    const payload = {
+      sub: user.id,
+      email: user.email,
+      role: user.role,
+      nickName: user.nickName,
+    };
     return {
       access_token: this.jwtService.sign(payload),
       user: {
